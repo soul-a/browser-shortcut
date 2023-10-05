554187e42c07ae824cc31828cac45e58a261059a
Ryan de Souza <64877708+soul-a@users.noreply.github.com>
2023-10-05 08:21:14 -0300

    fix home page on cleanup events

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -46,7 +46,7 @@ export default function Home() {
     shortcutHandler.configure(shortcuts);
     shortcutHandler.subscribe();
 
-    () => shortcutHandler.unsubscribe();
+    return () => shortcutHandler.unsubscribe();
   }, [shortcuts]);
 
   return (
